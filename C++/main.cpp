b7c86db56dee009b48369e0ccf16af56b5828d00
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-16 18:35:37 +0200

    Write the game in C++

---
+++ C++/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  int number, guess, tries;
+
+  srand(time(NULL));
+  number = rand() % 100 + 1;
+
+  do {
+    std::cout << "Enter a guess between 1 to 100: ";
+    std::cin >> guess;
+    tries++;
+
+    if (guess > number)
+      std::cout << "Too high!" << std::endl;
+
+    else if (guess < number)
+      std::cout << "Too low!" << std::endl;
+
+    else
+      std::cout << "Congratulations! You guessed the number in " << tries
+                << " tries!" << std::endl;
+
+  } while (guess != number);
+
+  return 0;
+}
